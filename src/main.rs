9fc9263f8a0d814b634481860e51a7a1e08ca9c3
kalamalahala <solo.driver.bob@gmail.com>
2023-12-13 16:44:45 -0500

    chapter 2 complete

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+    // println!("Randomly generated number: {}", secret_number);
+    
+    loop {
+        println!("Input an integer guess:");
+        let mut guess = String::new();
+        
+        io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small..."),
+            Ordering::Equal => {
+                println!("You got it!");
+                break;
+            },
+            Ordering::Greater => println!("Too big..."),
+            
+        }
+    }
+    
+    // println!("You guessed: {}", guess)
+}
